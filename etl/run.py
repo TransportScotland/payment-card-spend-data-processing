b84174f1a297ab468b53f46283d2396d57b2ab9c
fpavlica <frpavlica@gmail.com>
2022-07-29 18:52:14 +0100

    file choice fix

--- etl/run.py
+++ etl/run.py
@@ -16,8 +16,8 @@ time0 = time.time()
 
 # call each of the files' relevant ETL function to read the file, transform it, and load into the database
 # first deal with the main data sets of card data
-file1.etl('data/file1_1e4.csv') # smaller file for testing purposes
-# file1.etl('data/file1.csv')
+# file1.etl('data/file1_1e4.csv') # smaller file for testing purposes
+file1.etl('data/file1.csv')
 file2.etl('data/file2.csv')
 file3.etl('data/file3.csv')
 file4.etl('data/file4.csv')
@@ -25,8 +25,8 @@ file4.etl('data/file4.csv')
 # combine with other datasets
 # TODO add --reload-distances flag (or other datasets) to not load in distances every time (default False, maybe also do a check if exists in db)
 census_data.etl('other_data/KS101SC.csv')
-# distances.etl(['generated_data/durations_matrix.csv']) # needs to be run after census
-distances.etl(['generated_data/durations_matrix_20rows.csv']) # small subset for testing purposes
+distances.etl(['generated_data/durations_matrix.csv']) # needs to be run after census
+# distances.etl(['generated_data/durations_matrix_20rows.csv']) # small subset for testing purposes
 
 # save the dimensions into the database. This needs to be called, otherwise the database will have only the fact tables
 shared_funcs.save_dims()
